ae136cd07ff0094227ca686f99d5e8bdd713fa1b
2Anonymous <midyasukdeb111@gmail.com>
2021-04-22 01:06:20 +0530

    Dynamiclly set data in Rooms.

--- app/src/main/java/com/sochat/activity/HomeFragment.java
+++ app/src/main/java/com/sochat/activity/HomeFragment.java
@@ -1,19 +1,39 @@
 package com.sochat.activity;
 
+import android.content.Context;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.service.autofill.UserData;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.sochat.R;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
 public class HomeFragment extends Fragment {
 
        private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+
+    RecyclerView recyclerView;
+    ArrayList<String> roomname =new ArrayList<>();
+    ArrayList<String> announcment =new ArrayList<>();
+    ArrayList<String> members =new ArrayList<>();
 
 
     private String mParam1;
@@ -47,5 +67,45 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        recyclerView=(RecyclerView)getActivity().findViewById(R.id.recycler_GroupAdopter);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        try {
+            JSONObject jsonObject = new JSONObject(JsonDataFromAssets());
+            JSONArray jsonArray = jsonObject.getJSONArray("ROOMS");
+            for (int i=0;i<jsonArray.length();i++){
+                JSONObject roomsdata=jsonArray.getJSONObject(i);
+                roomname.add(roomsdata.getString("roomname"));
+                announcment.add(roomsdata.getString("announcment"));
+                members.add(roomsdata.getString("members"));
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        RoomAdapter roomAdapter= new RoomAdapter(roomname,announcment,members,HomeFragment.this);
+        recyclerView.setAdapter(roomAdapter);
+    }
+
+    private String JsonDataFromAssets() {
+        String json=" ";
+        try {
+            InputStream assetManager = getActivity().getAssets().open("rooms.json");
+            int sizeOfFile= assetManager.available();
+            byte[] bufferdata= new byte[sizeOfFile];
+            assetManager.read(bufferdata);
+            assetManager.close();
+            json=new String(bufferdata,"UTF-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return json;
     }
 }
---
+++ app/src/main/java/com/sochat/activity/RoomAdapter.java
@@ -0,0 +1,62 @@
+package com.sochat.activity;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.sochat.R;
+
+import java.util.ArrayList;
+
+public class RoomAdapter extends RecyclerView.Adapter<RoomAdapter.ViewClass> {
+
+    ArrayList<String> roomname;
+    ArrayList<String> announcment;
+    ArrayList<String> members;
+    Context context;
+
+    public RoomAdapter(ArrayList<String> roomname, ArrayList<String> announcment, ArrayList<String> members, HomeFragment context) {
+        this.roomname = roomname;
+        this.announcment = announcment;
+        this.members = members;
+        this.context=context;
+    }
+
+    @NonNull
+    @Override
+    public ViewClass onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.rooms,parent,false);
+        ViewClass viewClass=new ViewClass(view);
+        return viewClass;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewClass holder, int position) {
+        holder.roomname.setText(roomname.get(position));
+        holder.announcment.setText(announcment.get(position));
+        holder.members.setText(members.get(position));
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return roomname.size();
+    }
+
+    public class ViewClass extends RecyclerView.ViewHolder {
+        TextView roomname,announcment,members;
+
+        public ViewClass(@NonNull View itemView) {
+            super(itemView);
+            roomname=(TextView)itemView.findViewById(R.id.tv_RoomTitle);
+            announcment=(TextView)itemView.findViewById(R.id.tv_RoomAnnounus);
+            members=(TextView)itemView.findViewById(R.id.tv_NoOfMembers);
+        }
+    }
+}
