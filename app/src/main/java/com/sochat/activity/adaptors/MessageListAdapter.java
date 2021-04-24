90efef1e3e223f359efff526edc79a1dc0ead90f
Deepak Kumar Sharma <deepak.khandal3@gmail.com>
2021-04-25 01:44:33 +0530

    Correct layout

--- app/src/main/java/com/sochat/activity/adaptors/MessageListAdapter.java
+++ app/src/main/java/com/sochat/activity/adaptors/MessageListAdapter.java
@@ -55,11 +55,11 @@ public class MessageListAdapter extends RecyclerView.Adapter {
 
         if (viewType == VIEW_TYPE_MESSAGE_SENT) {
             view = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.item_message_sent, parent, false);
+                    .inflate(R.layout.message_sent, parent, false);
             return new SentMessageHolder(view);
         } else if (viewType == VIEW_TYPE_MESSAGE_RECEIVED) {
             view = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.item_message_received, parent, false);
+                    .inflate(R.layout.message_recived, parent, false);
             return new ReceivedMessageHolder(view);
         }
 
